709791087e682b1ca489b7698d055ec2ab5f54be
Ahemd <ahmedsaye23156@gmail.com>
2023-03-09 22:37:51 +0200

    adding a new styles

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function randomWholeNum() {
-
+function randomRange(myMin, myMax) {
     // Only change code below this line
-
-    return Math.floor(Math.random() * 5)
+    return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+    // Only change code above this line
 }
-console.log(randomWholeNum());
+
+console.log(randomRange(10, 15));
