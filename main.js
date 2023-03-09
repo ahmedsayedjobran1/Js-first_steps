c094e3562b38f781695737379a0313909f34d47c
Ahemd <ahmedsaye23156@gmail.com>
2023-03-09 23:09:41 +0200

    new lines

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function convertToInteger(str) {
-    return parseInt(str, 2)
+// Only change code below this line
+function countdown(n) {
+    if (n < 1) {
+        return [];
+    } else {
+        const arr = countdown(n - 1);
+        arr.unshift(n);
+        return arr;
+    }
 }
-
-console.log(convertToInteger("10011"));
+console.log(countdown(10));
