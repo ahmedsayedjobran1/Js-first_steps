26e8d7c50720042c434c75acd0cdf630da2103a8
Ahemd <ahmedsaye23156@gmail.com>
2023-03-11 17:30:19 +0200

    new lines

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-// one//
-document.getElementById("p1-name").textContent = "Ahmed";
-document.getElementById("p2-name").textContent = "NADA";
+//  All of these are strings
 
+/*
+- "Hello"
+-"I like single-quotes"
+-`I ❤️ backticks`
+-'867-5309'
 
-// change the palyers  name to you&neighbor
-document.getElementById("p1-symbol").textContent = "O";
-document.getElementById("p2-symbol").textContent = "x";
-document.querySelector("h1:nth-of-type(2)").textContent = "A game you know and love";
-// swap the players symbols
+*/ 
