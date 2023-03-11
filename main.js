7f05b4fc84f3ebe2a4bfe21a455b99f25576a83d
Ahemd <ahmedsaye23156@gmail.com>
2023-03-11 11:24:20 +0200

    addinng a new console.log files

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 //  thank you
 
 let one = document.querySelectorAll("p");
-console.log(one);
+console.log(one);  // first task
+
+let two = document.getElementById("p1-symbol").textContent;
+console.log(two);
