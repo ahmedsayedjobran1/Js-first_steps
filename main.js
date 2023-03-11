7b20c3194639fa6487e5b232153af2753a2773d2
Ahemd <ahmedsaye23156@gmail.com>
2023-03-11 12:09:52 +0200

    new other world

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-//  thank you
+document.title = "my page";
 
-let one = document.querySelectorAll("p");
-console.log(one);  // first task
-
-let two = document.getElementById("p1-symbol").textContent;
-console.log(two);
-// second task
-let three = document.querySelectorAll(".square").length;
-console.log(three);
-//  third task
-let four = document.querySelector("h1:nth-of-type(1)").textContent;
-console.log(four);
+document.getElementById("p1-name").append(" and Ahmed:)");
+document.getElementById("p2-name").append(" himslef:)")
