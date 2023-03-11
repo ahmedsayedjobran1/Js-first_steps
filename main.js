f5add1100a0cdc295496e64334455fd6ce278cdb
Ahemd <ahmedsaye23156@gmail.com>
2023-03-11 16:23:41 +0200

    new other line

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
+// one//
+document.getElementById("p1-name").textContent = "Ahmed";
+document.getElementById("p2-name").textContent = "NADA";
 
 
 // change the palyers  name to you&neighbor
-document.getElementById("p1-name").textContent = "Ahmed";
-document.getElementById("p2-name").textContent = "Ashraf";
+document.getElementById("p1-symbol").textContent = "O";
+document.getElementById("p2-symbol").textContent = "x";
+document.querySelector("h1:nth-of-type(2)").textContent = "A game you know and love";
 // swap the players symbols
-Ahmed = document.querySelector("#p1-symbol");
-Ashraf = document.querySelector("#p2-symbol");
-
-Ahmed.textContent = "O";
-Ashraf.textContent = "X";
-
-document.querySelector("h2").textContent = "A game you know and  love "
