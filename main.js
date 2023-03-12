5fef705cc170bcc4b305f91eaff42e96192ad038
Ahemd <ahmedsaye23156@gmail.com>
2023-03-12 19:12:41 +0200

    what sort is doung

--- main.js
+++ main.js
@@ -2,3 +2,7 @@
 //  unshift  add the first elemt
 //  pop remove thwe last element
 //  push : add to the last element
+
+let one = ["a", "b", "d", "c"].sort().reverse();
+console.log(one);   // arrange it from the smaller to the bigger
+// like if you have [1,2,4,5];
