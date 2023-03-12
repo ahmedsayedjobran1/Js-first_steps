400e2c1b3c5fdeeb49d9403fdea8e7271124cb61
Ahemd <ahmedsaye23156@gmail.com>
2023-03-12 11:57:29 +0200

    new

---
+++ main.js
@@ -0,0 +1,16 @@
+let Esraa = {
+    name: "esraa",
+    age: "19",
+}
+let maryam = {
+    name: "maryam",
+    age: 18
+}
+let hagar = {
+    name: "hagar",
+    age: 10
+}
+let hourYouAwake = [18]
+let averageHours = 24 - hourYouAwake
+console.log(Number(Esraa.age) + maryam.age + hagar.age);
+console.log(averageHours);
