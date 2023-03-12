e929726c94563661886c1d44339a953ef1eab3b0
Ahemd <ahmedsaye23156@gmail.com>
2023-03-12 22:35:26 +0200

    new lines

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-let student1 = "Ahmed";
-student2 = student1;
+// let student1 = "Ahmed";
+// student2 = student1;
 
-console.log(student2);
-student1 = "Nada";
-console.log(student1);
-console.log(student2);
+// console.log(student2);
+// student1 = "Nada";
+// console.log(student1);
+// console.log(student2);
+const num1 = [1, 2, 3, 4]; /// I thought it's a const varaible which it's not change ever
+
+num1[3] = "3";
+console.log(num1);
