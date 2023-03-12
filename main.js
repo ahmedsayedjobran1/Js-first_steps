9a0962a32149337bdb99f0bf47b7f46cffa44b9e
Ahemd <ahmedsaye23156@gmail.com>
2023-03-12 13:18:03 +0200

    a

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-console.log(1 === 1);
-console.log(1 == 1);
-console.log("1" === "1");
-console.log("1" == "1");
-console.log("1" == 1);  // it's going to give us a  trur cause it's just judge the value not the type
-
-console.log("1" === 1); // this is going to give us a false value
+let Ahmed = {
+    nickName: "AhmedSayed",
+    age: 21
+}
+console.log(Ahmed.nickName.includes("S+a"));
