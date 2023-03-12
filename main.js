67caf4db087903d87b940366e232c93b4870da80
Ahemd <ahmedsaye23156@gmail.com>
2023-03-12 22:33:07 +0200

    adding a new lines i  the github

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-const A = [1, 2, 3, 4, 5];
-A.push(6);
-console.log(A);  // when we use an immutable varaible it will chage when we sue a muttable value like push
+let student1 = "Ahmed";
+student2 = student1;
+
+console.log(student2);
+student1 = "Nada";
+console.log(student1);
+console.log(student2);
