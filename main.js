9fa40194176b9702ecd7536d0a55beae8e82b66c
Ahemd <ahmedsaye23156@gmail.com>
2023-03-12 12:45:45 +0200

    new

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 console.log(1 === 1);
 console.log(1 == 1);
-console.log("1"==="1");
-console.log("1"=="1");
+console.log("1" === "1");
+console.log("1" == "1");
+console.log("1" == 1);
+console.log("1" === 1); // this is going to give us a false value
