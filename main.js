6c23d4fea1d5c0b07a1b58b5e368493eec0367b2
Ahemd <ahmedsaye23156@gmail.com>
2023-03-12 15:26:22 +0200

    test

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-//
-let ten = 6 + 4;
- myDecrale = "new value";
-// let board = document.getElementById("board");
-console.log(myDecrale);
+// fucntions
+function add(x, y) {
+    return x + y;
+}
+let biggest;
+if (5 > 4) {
+    biggest = 5;
+}
+else {
+    biggest = 4;
+}
+
+for (let key of "Ahmed") {
+    console.log(key)
+}
