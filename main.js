df9ff701e9855bb9efb31450c1deeef5155b4127
Ahemd <ahmedsaye23156@gmail.com>
2023-03-12 13:02:13 +0200

    the aim is to reaach 50 commits

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ console.log(1 === 1);
 console.log(1 == 1);
 console.log("1" === "1");
 console.log("1" == "1");
-console.log("1" == 1);
+console.log("1" == 1);  // it's going to give us a  trur cause it's just judge the value not the type
+
 console.log("1" === 1); // this is going to give us a false value
