ec7b4e199acb2b46964fc967644f52ae9ef02ef0
Ahemd <ahmedsaye23156@gmail.com>
2023-03-12 19:57:43 +0200

    new otehr one

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-let ABCString = "abc";
-// ABCString[1] = "d";
-console.log(ABCString.indexOf("a"));
+let num1 = [1, 2, 3];
+num1.push(4);
+console.log(num1);  // what it is going to do here :[1,2,3,4]
+
+let num2 = [1, 2, 3];
+console.log(num2.concat([4]));  // it's going to give me :[1,2,3,4]
+let c = num2.concat[4];
