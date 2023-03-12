bfacdcd7954d1f0c67223a98fd76bc4affd6ab17
Ahemd <ahmedsaye23156@gmail.com>
2023-03-12 18:02:50 +0200

    new

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 let syn = ["plethora", "array", "cornucopia"];
 
 console.log(syn[1].indexOf("cornucopia"));  // then it's not contain an  cornucopia
+console.log(syn[3]);  // it's going to give me an undefiend valueg
