da69b02309813c0459384b9f3b38e1de059bb884
Ahemd <ahmedsaye23156@gmail.com>
2023-03-13 12:26:11 +0200

    ooops objects are awesome

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-// objects
-
-const js = {
-    name: "JavaScript",
-    abbrevation: "JS",
-    isAwesome: true,
-    officialSpec: "EcmaScript",
-    birthYear: 1995,
-    creator: "Brenden Eich "
-}
-
-console.log(js.abbrevation);// this  is going to give us the apprevation
-console.log(js.isAwesome) // this is going to print out the value of the isAwesome boelen value
-console.log(js.creator + "His real name is ahmed")  // this is going to print out the value of the print out value 
-console.log(js["birthYear"])  // this is going to give us the birthyear which you are born in 
-console.log(js.name.startsWith("Java"));
-let age = 2023 - (js.birthYear);
-console.log(age); // awesome js has 28 years old 
+let Ahmed = {};
+Ahmed.age = 21;
+Ahmed.gender = "male";
+Ahmed.gf = "none";
+console.log(Ahmed);
