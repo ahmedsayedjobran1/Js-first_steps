52d251df0a0fe2be16191c9212857453ba7258a8
Ahemd <ahmedsaye23156@gmail.com>
2023-03-13 14:57:08 +0200

    new

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-let Ahmed = { name: "Ahmed" };
-Ahmed.speak = function () {
-    console.log("hi my name is", this.name)
-}
+const spices = [
+    { name: "Emma", nickname: "baby" },
+    { name: "Geri", nickname: "Ginger" },
+    { name: "Mel B", nickname: "Scary" },
+    { name: "Mel C", nickname: "sporty" }
+];
 
-Ahmed.speak(); // this refers to the Ahmed object
+let spiceGirls = {
+    albums: ["Spice", "Spiceworld", "Forever"],
+    motto: "Girl power",
+    members: spices
+};
+// Girl Power
+let Girl_power = spiceGirls.motto;
+let Ginger_spice = spiceGirls.members;
+let Spiceworld = spiceGirls.albums[1];
+spiceGirls.albums[3] = "Victoria";
+console.log(Girl_power);
+console.log(Ginger_spice);
+console.log(Spiceworld);
+console.log(spiceGirls.albums);
