1b1cb687142764777d9bddfe2db3ea5e1fc8429f
Ahemd <ahmedsaye23156@gmail.com>
2023-03-13 15:31:15 +0200

    this is going to give us  the number between 0 and 6)

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-const spices = [
-    { name: "Emma", nickname: "baby" },
-    { name: "Geri", nickname: "Ginger" },
-    { name: "Mel B", nickname: "Scary" },
-    { name: "Mel C", nickname: "sporty" },
-    { name: "Victoria", nickname: "Posh" },
-];
+// let pi = Math.PI;
+// console.log(pi);
 
-let spiceGirls = {
-    albums: ["Spice", "Spiceworld", "Forever"],
-    motto: "Girl power",
-    members: spices
-};
-// Girl Power
-let Girl_power = spiceGirls.motto;
-let Ginger_spice = spiceGirls.members;
-let Spiceworld = spiceGirls.albums[1];
-console.log(Girl_power);
-console.log(Ginger_spice);
-console.log(spiceGirls.members[4].name);  //  it's most complicated 
-
+let ran = Math.floor(Math.random() * 6)
+console.log(ran);
