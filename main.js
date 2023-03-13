132d97dd7b880e917af20ed97bb13fa8a3fac464
Ahemd <ahmedsaye23156@gmail.com>
2023-03-13 12:07:03 +0200

    new other lands

--- main.js
+++ main.js
@@ -10,4 +10,5 @@ const js = {
 }
 
 console.log(js.abbrevation);// this  is going to give us the apprevation
-console.log(js.isAwesome)
+console.log(js.isAwesome) // this is going to print out the value of the isAwesome boelen value
+console.log(js.creator + "His real name is ahmed")
