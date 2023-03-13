4e62e62c563285ceacf4ce6af4cc97ba437ebc92
Ahemd <ahmedsaye23156@gmail.com>
2023-03-13 12:09:13 +0200

    new

--- main.js
+++ main.js
@@ -11,4 +11,5 @@ const js = {
 
 console.log(js.abbrevation);// this  is going to give us the apprevation
 console.log(js.isAwesome) // this is going to print out the value of the isAwesome boelen value
-console.log(js.creator + "His real name is ahmed")
+console.log(js.creator + "His real name is ahmed")  // this is going to print out the value of the print out value 
+console.log(js["birthYear"])  // this is going to give us the birthyear which you are born in 
