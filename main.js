aefc1d6df311d7e247cc680d565b24e3e5b04aba
Ahemd <ahmedsaye23156@gmail.com>
2023-03-13 12:03:28 +0200

    new

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-// let student1 = "Ahmed";
-// student2 = student1;
+// objects
 
-// console.log(student2);
-// student1 = "Nada";
-// console.log(student1);
-// console.log(student2);
-const num1 = [1, 2, 3, 4]; /// I thought it's a const varaible which it's not change ever
+const js = {
+    name: "JavaScript",
+    abbrevation: "JS",
+    isAwesome: true,
+    officialSpec: "EcmaScript",
+    birthYear: 1995,
+    creator: "Brenden Eich "
+}
 
-// num1[3] = "3";
-// console.log(num1);
-num1[0] = "Ahmed";
-
-const num2 = num1;
-console.log(num2);
+console.log(js.name);
