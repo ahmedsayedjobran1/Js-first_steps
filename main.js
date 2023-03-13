b7388740a6a45893abb906999198695ec1c97cbd
Ahemd <ahmedsaye23156@gmail.com>
2023-03-13 13:44:41 +0200

    otherstuffs

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const Ahmed = {
-    name: "Ahmed jobran",
+const me = {
+    name: "Ahmed",
     age: 21,
-    interists: ["video games", "reading books", "chess"],
-    gender: "male",
-    favFilms: "john  wick",
-
+    gender: "female"
 }
-
-console.log(Ahmed.interists[0][2]);
+Object.freeze(me); // this is going to change it to none thing 
+me.age = 35;  // this is not going to change any thing you will change 
+me.name = "hani";  // this is not going to change any thing you will change 
+console.log(me);
