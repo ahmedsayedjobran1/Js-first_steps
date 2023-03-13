95f308eb94cac7c21e189c6a85c4f2e4acc1cafd
Ahemd <ahmedsaye23156@gmail.com>
2023-03-13 16:17:03 +0200

    new

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-// let pi = Math.PI;
-// console.log(pi);
+let PlayerOne = document.querySelector("#p1-name").textContent;
+let Playertwo = document.querySelector("#p2-name").textContent;
+let Symbol1 = document.querySelector("#p1-symbol").textContent;
+let Symbol2 = document.querySelector("#p2-symbol").textContent;
 
-let ran = Math.floor(Math.random() * 6)
-console.log(ran);
+let Players = [{ Name: PlayerOne, Symbol: Symbol1 },
+{ Name: Playertwo, Symbol: Symbol2 }]
+
+Players.push({ Name: "NADA", Symbol: "O" }, { Name: "Ahmed", Symbol: "X" });
+// Playertwo = Players[Players.length - 1].Name;
+document.querySelector("#p2-name").textContent = Players[Players.length - 2].Name;
+document.querySelector("#p1-name").textContent = Players[Players.length - 1].Name;
+
+// console.log(Playertwo);
+// Playertwo.textContent
+
+// console.log(Playertwo);
